fff2f83dc97f3a927405139c45a38ebc30a87ff8
Soham Bhardwaj <53705102+DigBick17@users.noreply.github.com>
2020-04-20 14:45:40 +0530

    Compare Upvotes on posts

--- Compare Recent Reddit Upvotes.py
+++ Compare Recent Reddit Upvotes.py
@@ -3,11 +3,19 @@ import requests
 user1=input("Enter First User ")
 user2=input("Enter Second User ")
 r1=requests.get('https://www.reddit.com/user/%s.json' % user1,headers = {'User-agent': 'wtf 0.1'})
-r2=requests.get('https://www.reddit.com/user/%s.json' % user2,headers = {'User-agent': 'wtf 0.1'})
+r2=requests.get('https://www.reddit.com/user/%s.json' % user2,headers = {'User-agent': 'w 0.1'})
 data1=r1.json()
 data2=r2.json()
-ups1=data1["data"]['children'][0]["data"]['ups']
-ups2=data2["data"]['children'][0]['data']['ups']
+
+for item in data1["data"]['children']:
+	if item["kind"]=="t3":
+		ups1=item["data"]['ups']
+		break
+for Item in data2["data"]['children']:
+	if Item["kind"]=="t3":
+		ups2=Item["data"]['ups']
+		break
+
 if ups1>ups2:
 	print(user1,"got more up votes")
 else:
